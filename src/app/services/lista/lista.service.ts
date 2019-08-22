803fadf1218101ab80261dc292a305f41bef4c9d
John Theo <john.theo.souza@gmail.com>
2019-08-21 21:23:33 -0300

    Criacao do service Lista

--- src/app/services/lista/lista.service.ts
+++ src/app/services/lista/lista.service.ts
@@ -1,9 +1,82 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestoreCollection, AngularFirestore, QueryDocumentSnapshot } from 'angularfire2/firestore';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
+export interface Lista{
+  nome: string;
+  cor: string;
+  items: number;
+  data_criacao: Date; 
+}
 @Injectable({
   providedIn: 'root'
 })
 export class ListaService {
+  private listasCollectiion: AngularFirestoreCollection<Lista>;
+  private listas: Observable<Lista[]>;
+  private lastSeen: QueryDocumentSnapshot<Lista>;
+  private LIMIT: number = 10;
+  public db: AngularFirestore;
 
-  constructor() { }
+  constructor(db: AngularFirestore) { 
+    this.db = db;
+    this.listasCollectiion = db.collection<Lista>('listas',ref => ref.orderBy('data_criacao').limit(this.LIMIT));
+
+    this.listas = this.listasCollectiion.snapshotChanges().pipe(
+      map(actions => {
+        //ULTIMO LISTA
+        this.lastSeen = actions[this.LIMIT - 1].payload.doc;
+
+        return actions.map(a => {
+          const data = a.payload.doc.data();
+          const id = a.payload.doc.id;
+          return { id, ...data };
+        });
+      })
+    );
+  }
+
+  getListas(){
+    return this.listas;
+  }
+
+  getMoreListas(){
+    var novasListasCollection = this.db.collection<Lista>('listas', ref => ref.orderBy('data_criacao').startAfter(this.lastSeen).limit(this.LIMIT));
+
+    var novasListas = novasListasCollection.snapshotChanges().pipe(
+      map(actions => {
+        if (actions.length > 0) {
+          //ULTIMO LISTA
+          this.lastSeen = actions[actions.length-1].payload.doc;
+          return actions.map(a => {
+            const data = a.payload.doc.data();
+            const id = a.payload.doc.id;
+            return { id, ...data };
+          });
+        }
+        else{
+          return null;
+        }
+      })
+    );
+
+    return novasListas;
+  }
+
+  getLista(id: string) {
+    return this.listasCollectiion.doc<Lista>(id).valueChanges();
+  }
+
+  updateLista(evento: Lista, id: string) {
+    return this.listasCollectiion.doc(id).update(evento);
+  }
+
+  addLista(evento: Lista) {
+    return this.listasCollectiion.add(evento);
+  }
+
+  removeLista(id: string) {
+    return this.listasCollectiion.doc(id).delete();
+  }
 }
