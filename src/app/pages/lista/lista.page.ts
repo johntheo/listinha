ec4cc04f7079f917121630768566f2827e003995
John Theo <john.theo.souza@gmail.com>
2019-08-29 01:51:30 -0300

    Contador de items. Segment com problema

--- src/app/pages/lista/lista.page.ts
+++ src/app/pages/lista/lista.page.ts
@@ -12,17 +12,18 @@ import { Observer, Observable } from 'rxjs';
   styleUrls: ['./lista.page.scss'],
 })
 export class ListaPage implements OnInit {
+  status: string = "pendentes";
 
   id: string;
   listaRef: AngularFirestoreDocument;
   itemsPendentesRef: AngularFirestoreCollection;
   itemsFinalizadosRef: AngularFirestoreCollection;
 
-  lista:Observable<Lista>;
-  itemsPendentes:Observable<ToDoItem[]>;
-  itemsFinalizados:Observable<ToDoItem[]>;
+  lista: Observable<Lista>;
+  itemsPendentes: Observable<ToDoItem[]>;
+  itemsFinalizados: Observable<ToDoItem[]>;
 
-  item:ToDoItem = {
+  item: ToDoItem = {
     prioridade: 1,
     nome: '',
     finalizado: false
@@ -35,24 +36,38 @@ export class ListaPage implements OnInit {
 
     this.listaRef = this.afs.doc(`listas/${this.id}`);
     this.lista = this.firestoreService.get<Lista>(this.listaRef);
-    
+
     this.itemsPendentesRef = this.afs.collection(`listas/${this.id}/items`, ref => ref.where('finalizado', '==', false));
     this.itemsPendentes = this.firestoreService.list<ToDoItem>(this.itemsPendentesRef)
-    
+
     this.itemsFinalizadosRef = this.afs.collection(`listas/${this.id}/items`, ref => ref.where('finalizado', '==', true));
     this.itemsFinalizados = this.firestoreService.list<ToDoItem>(this.itemsFinalizadosRef)
 
   }
 
-  toggleItem(item:ToDoItem){
+  toggleItem(item: ToDoItem) {
     item.finalizado = !item.finalizado;
     let itemRef = this.afs.doc(`listas/${this.id}/items/${item.id}`);
-    this.firestoreService.update(itemRef,item);
+    this.firestoreService.update(itemRef, item).then(() => this.atualizarContadorLista());
   }
 
-  adicionarItem(){
+  adicionarItem() {
     //console.log(this.item);
-    this.firestoreService.add(this.itemsPendentesRef,this.item).then(() => this.limparItem())
+    this.firestoreService.add(this.itemsPendentesRef, this.item).then(() => this.limparItem()).then(() => this.atualizarContadorLista());
+  }
+
+  deletarItem(item: ToDoItem) {
+    let itemRef = this.afs.doc(`listas/${this.id}/items/${item.id}`);
+    this.firestoreService.delete(itemRef).then(() => this.atualizarContadorLista());
+    //TODO: Toast
+  }
+
+  onSwipe(item: ToDoItem, event) {
+    if (event.detail.side == "start") {
+      this.deletarItem(item);
+    } else if (event.detail.side == "end") {
+      this.toggleItem(item);
+    }
   }
 
   limparItem() {
@@ -61,5 +76,15 @@ export class ListaPage implements OnInit {
       nome: '',
       finalizado: false
     }
+  }
+
+  atualizarContadorLista() {
+    return this.itemsPendentes.subscribe(res => {
+      if(res){
+        return this.firestoreService.update(this.listaRef, { items: res.length });
+      }else{
+        return this.firestoreService.update(this.listaRef, { items: 0 })
+      }
+    })
   }
 }
